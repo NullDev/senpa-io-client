34d1e40c6deb37e81331b43c06e595674cbef6cd
NullDev (Chris) <shadow@nulldev.org>
2022-06-26 20:33:05 +0200

    added modules

---
+++ src/modules/cliSwitches.js
@@ -0,0 +1,70 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+/**
+ * Apply CLI switches for Chromium.
+ * Some for performance, some for security and some for development.
+ *
+ * @class CliSwitches
+ */
+class CliSwitches {
+    /**
+     * List of switches to apply.
+     *
+     * @static
+     * @getter
+     * @returns {Array<Array<string>>}
+     * @memberof CliSwitches
+     */
+    static get #switches(){
+        return [
+            ["autoplay-policy", "no-user-gesture-required"],
+            ["force_high_performance_gpu"],
+            ["disable-frame-rate-limit"],
+            ["disable-gpu-vsync"],
+            ["disable-accelerated-2d-canvas", "true"],
+            ["ignore-gpu-blacklist"],
+            ["disable-breakpad"], // Disable crash reporting
+            ["disable-print-preview"],
+            ["max-active-webgl-contexts", "100"],
+            ["enable-quic"],
+            ["high-dpi-support", "1"],
+            ["disable-2d-canvas-clip-aa"],
+            ["enable-highres-timer"], // high resolution timer -> less input delay (mostly)
+            ["disable-metrics"],
+            ["disable-metrics-repo"],
+            ["disable-bundled-ppapi-flash"],
+            ["disable-logging"],
+            ["enable-javascript-harmony"],
+            ["enable-future-v8-vm-features"],
+            ["disable-low-end-device-mode"],
+            ["enable-webgl2-compute-context"],
+            ["disable-hang-monitor"],
+            ["renderer-process-limit", "100"],
+            ["disable-web-security"],
+            ["webrtc-max-cpu-consumption-percentage=100"],
+            ["enable-zero-copy"],
+            ["no-pings"],
+            ["no-proxy-server"],
+            ["disable-gpu-vsync"]
+        ];
+    }
+
+    /**
+     * Apply CLI switches.
+     *
+     * @static
+     * @param {import("electron").App} app
+     * @memberof CliSwitches
+     */
+    static applySwitches(app){
+        for (const [ option, value ] of CliSwitches.#switches){
+            app.commandLine.appendSwitch(option, value);
+        }
+    }
+}
+
+module.exports = CliSwitches;
---
+++ src/modules/windowBuilder.js
@@ -0,0 +1,184 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+const path = require("node:path");
+
+const { app, BrowserWindow, shell } = require("electron");
+const webBlocker = require("electron-web-blocker");
+
+/**
+ * Build browser window.
+ *
+ * @class WindowBuilder
+ */
+class WindowBuilder {
+    /**
+     * List of window options.
+     *
+     * @static
+     * @getter
+     * @returns {Object}
+     * @memberof WindowBuilder
+     */
+    static get #mainWindowOptions(){
+        return {
+            width: 1600,
+            height: 950,
+            autoHideMenuBar: true,
+            show: false,
+            center: true,
+            title: "Senpa.io :: Official Client",
+            fullscreenable: true,
+            webPreferences: {
+                preload: path.join(__dirname, "../scripts/preload.js"),
+                contextIsolation: false,
+                nodeIntegration: false,
+                spellcheck: false,
+                devTools: false
+            }
+        };
+    }
+
+    /**
+     * List of window options.
+     *
+     * @static
+     * @getter
+     * @returns {Object}
+     * @memberof WindowBuilder
+     */
+    static get #spawnWindowOptions(){
+        return {
+            width: 600,
+            height: 300,
+            center: true,
+            resizable: false,
+            show: false,
+            frame: false,
+            fullscreenable: false,
+            transparent: true,
+            webPreferences: {
+                contextIsolation: false,
+                nodeIntegration: false,
+                spellcheck: false,
+                devTools: false
+            }
+        };
+    }
+
+    /**
+     * Initialize the web blocker.
+     *
+     * @static
+     * @returns {Promise<any>}
+     * @memberof WindowBuilder
+     */
+    static #initBlocker(){
+        return webBlocker.init({
+            updateNow: true,
+            blacklist: [
+                "doubleclick.net",
+                "*.doubleclick.net",
+                "doubleclick.com",
+                "*.doubleclick.com",
+                "googlesyndication.com",
+                "*.googlesyndication.com",
+                "gameads.io",
+                "*.gameads.io",
+                "googleadservices.com",
+                "*.googleadservices.com",
+                "adservice.google.*",
+                "adsense.google.com",
+                "doubleclickbygoogle.com",
+                "*.doubleclickbygoogle.com"
+            ]
+        });
+    }
+
+    /**
+     * Create the splash screen.
+     *
+     * @static
+     * @returns {BrowserWindow}
+     * @memberof WindowBuilder
+     */
+    static #splashScreen(){
+        const win = new BrowserWindow(WindowBuilder.#spawnWindowOptions);
+        win.loadFile(path.join(__dirname, "..", "layout", "splash.html"));
+        return win;
+    }
+
+    /**
+     * Create the main screen.
+     *
+     * @static
+     * @returns {BrowserWindow}
+     * @memberof WindowBuilder
+     */
+    static #mainWindow(){
+        const win = new BrowserWindow(WindowBuilder.#mainWindowOptions);
+
+        win.removeMenu();
+        win.setMenuBarVisibility(false);
+        win.loadURL("https://senpa.io/web", {
+            extraHeaders: `x-senpa-io-client-version: ${app.getVersion()}\n`
+        });
+
+        webBlocker.filter(win);
+
+        win.once("close", () => win.destroy());
+
+        win.webContents.on("will-navigate", (event, url) => {
+            event.preventDefault();
+            (url.includes("senpa.io") && !url.endsWith("privacy.txt") && !url.endsWith("terms.txt"))
+                ? win.loadURL(url)
+                : shell.openExternal(url);
+        });
+
+        return win;
+    }
+
+    /**
+     * Wait for a given amount of milliseconds.
+     *
+     * @static
+     * @returns {Promise<any>}
+     * @memberof WindowBuilder
+     */
+    static #sleep(ms){
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    /**
+     * Prepare and create the browser windows.
+     *
+     * @static
+     * @memberof WindowBuilder
+     */
+    static createWindow(){
+        const splash = WindowBuilder.#splashScreen();
+        const main = WindowBuilder.#mainWindow();
+
+        splash.once("ready-to-show", () => {
+            splash.show();
+            splash.focus();
+
+            // Wait for blocker to be initialized.
+            // Or, if it initializes "too fast",
+            // wait 3 seconds to show the splash screen.
+            Promise.all([
+                WindowBuilder.#sleep(3000),
+                WindowBuilder.#initBlocker()
+            ]).then(() => {
+                splash.close();
+                main.show();
+                main.focus();
+            });
+        });
+    }
+}
+
+module.exports = WindowBuilder;
